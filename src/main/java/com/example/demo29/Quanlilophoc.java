10f95538dce50fcb7cef7b34a9edc51d4893b473
tthuyddung <dungttt.23itb@vku.udn.vn>
2024-05-03 14:30:12 +0700

    Sửa đổi code

--- src/main/java/com/example/demo29/Quanlilophoc.java
+++ src/main/java/com/example/demo29/Quanlilophoc.java
@@ -1,22 +1,18 @@
 package com.example.demo29;
 
+import com.example.demo29.Student;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
@@ -48,11 +44,19 @@ public class Quanlilophoc {
     @FXML
     private Button addButton;
 
+    private ObservableList<Student> studentList;
+
     public void initialize() {
+        // Khởi tạo danh sách sinh viên
+        studentList = FXCollections.observableArrayList();
+
         // Khởi tạo cột trong bảng TableView
         studIDColumn.setCellValueFactory(cellData -> cellData.getValue().studIDProperty());
         nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
         addressColumn.setCellValueFactory(cellData -> cellData.getValue().addressProperty());
+
+        // Khôi phục dữ liệu từ lần chạy trước đó (nếu có)
+        restoreData();
     }
 
     @FXML
@@ -62,70 +66,105 @@ public class Quanlilophoc {
         String name = nameTextField.getText();
         String address = addressTextField.getText();
 
-        // Thêm thông tin vào bảng TableView
+        // Thêm sinh viên vào danh sách
         Student student = new Student(studID, name, address);
-        tableView.getItems().add(student);
+        studentList.add(student);
 
         // Lưu thông tin vào file XML
-        StudentXMLWriter writer = new StudentXMLWriter();
-        writer.saveToXML(student);
+        saveToXML(student);
+
+        // Xóa nội dung TextField sau khi thêm sinh viên thành công
+        studIDTextField.clear();
+        nameTextField.clear();
+        addressTextField.clear();
     }
 
-    public class StudentXMLWriter {
-        public void saveToXML(Student student) {
-            try {
-                // Kiểm tra nếu file XML đã tồn tại
-                File file = new File("sinhvien.xml");
-                boolean fileExists = file.exists();
+    private void saveToXML(Student student) {
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
-                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-                Document doc;
+            Document doc;
+            File file = new File("sinhvien.xml");
+            boolean fileExists = file.exists();
 
-                // Nếu file đã tồn tại, sử dụng nó để cập nhật dữ liệu
-                if (fileExists) {
-                    doc = docBuilder.parse(file);
-                } else {
-                    doc = docBuilder.newDocument();
-                    Element rootElement = doc.createElement("students");
-                    doc.appendChild(rootElement);
-                }
-
-                Element studentElement = doc.createElement("student");
-
-                Element studIDElement = doc.createElement("studID");
-                studIDElement.appendChild(doc.createTextNode(student.getStudID()));
-                studentElement.appendChild(studIDElement);
-
-                Element nameElement = doc.createElement("name");
-                nameElement.appendChild(doc.createTextNode(student.getName()));
-                studentElement.appendChild(nameElement);
-
-                Element addressElement = doc.createElement("address");
-                addressElement.appendChild(doc.createTextNode(student.getAddress()));
-                studentElement.appendChild(addressElement);
-
-                // Nếu file đã tồn tại, thêm thẻ sinh viên vào phần tử gốc
-                if (fileExists) {
-                    Node root = doc.getDocumentElement();
-                    root.appendChild(studentElement);
-                } else {
-                    // Nếu file chưa tồn tại, tạo file mới và thêm phần tử sinh viên vào
-                    Element rootElement = doc.getDocumentElement();
-                    rootElement.appendChild(studentElement);
-                }
-
-                TransformerFactory transformerFactory = TransformerFactory.newInstance();
-                Transformer transformer = transformerFactory.newTransformer();
-                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-
-                DOMSource source = new DOMSource(doc);
-                StreamResult result = new StreamResult(file);
-
-                transformer.transform(source, result);
-            } catch (ParserConfigurationException | TransformerException | IOException | SAXException e) {
-                e.printStackTrace();
+            if (fileExists) {
+                doc = docBuilder.parse(file);
+            } else {
+                doc = docBuilder.newDocument();
+                Element rootElement = doc.createElement("students");
+                doc.appendChild(rootElement);
             }
+
+            Element studentElement = doc.createElement("student");
+
+            Element studIDElement = doc.createElement("studID");
+            studIDElement.appendChild(doc.createTextNode(student.getStudID()));
+            studentElement.appendChild(studIDElement);
+
+            Element nameElement = doc.createElement("name");
+            nameElement.appendChild(doc.createTextNode(student.getName()));
+            studentElement.appendChild(nameElement);
+
+            Element addressElement = doc.createElement("address");
+            addressElement.appendChild(doc.createTextNode(student.getAddress()));
+            studentElement.appendChild(addressElement);
+
+            if (fileExists) {
+                Node root = doc.getDocumentElement();
+                root.appendChild(studentElement);
+            } else {
+                Element rootElement = doc.getDocumentElement();
+                rootElement.appendChild(studentElement);
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(file);
+
+            transformer.transform(source, result);
+        } catch (ParserConfigurationException | TransformerException | IOException | SAXException e) {
+            e.printStackTrace();
         }
     }
+
+    private void restoreData() {
+        try {
+            File file = new File("sinhvien.xml");
+            if (file.exists()) {
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+                Document doc = docBuilder.parse(file);
+
+
+                doc.getDocumentElement().normalize();
+                Element rootElement = doc.getDocumentElement();
+                NodeList nodeList = rootElement.getElementsByTagName("student");
+
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    Node node = nodeList.item(i);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element) node;
+                        String studID = element.getElementsByTagName("studID").item(0).getTextContent();
+                        String name = element.getElementsByTagName("name").item(0).getTextContent();
+                        String address = element.getElementsByTagName("address").item(0).getTextContent();
+
+                        Student student = new Student(studID, name, address);
+                        studentList.add(student);
+                    }
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+
+        // Hiển thị danh sách sinh viên trong bảng TableView
+        tableView.setItems(studentList);
+    }
+
+
+
 }
